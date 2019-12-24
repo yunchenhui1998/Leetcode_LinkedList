0f67ec968b64661de786a52d8895a58c3c5c6993
yunchenhui1998 <yunchenhui1998@163.com>
2019-12-24 22:26:14 +0800

    2019/22/24/22:26

---
+++ src/com/xupt/practice/DoublePointer/PalindromeLinkedList.java
@@ -0,0 +1,40 @@
+package com.xupt.practice.DoublePointer;
+/*
+回文链表
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ */
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head,fast=head;
+        ListNode pre=null;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            ListNode temp=slow.next;
+            slow.next=pre;
+            pre=slow;
+            slow=temp;
+        }
+        if(fast!=null){
+            slow=slow.next;
+        }
+        while(pre!=null&&slow!=null){
+            if(pre.val!=slow.val){
+                return false;
+            }
+            slow=slow.next;
+            pre=pre.next;
+        }
+        return true;
+    }
+}
